5681e49c590f5e98e565d34cc12e725655307230
lester <lesterc013@gmail.com>
2024-07-09 10:44:40 +0800

    View and Hide for Blog component

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const hideWhenView = view ? { display: "none" } : { display: "" }
+  const showWhenView = view ? { display: "" } : { display: "none" }
+
+  const switchView = () => {
+    setView(!view)
+  }
+
+  return (
+    <div
+      style={{
+        whiteSpace: "pre-wrap",
+        border: "1px solid",
+        margin: "5px",
+        padding: "5px",
+      }}
+    >
+      <div style={hideWhenView}>
+        {blog.title}, {blog.author}
+        <button onClick={switchView}>view</button>
+      </div>
+
+      <div style={showWhenView}>
+        {blog.title}, {blog.author}
+        <button onClick={switchView}>hide</button>
+        {"\n"}
+        {blog.url}
+        {"\n"}
+        {blog.likes}
+        <button onClick={() => console.log("like pressed")}>like</button>
+        {"\n"}
+        {blog.user.name}
+        {"\n"}
+      </div>
+    </div>
+  )
+}
 
 export default Blog

807e012671903848778cabb79683a52e581ca4d6
lester <lesterc013@gmail.com>
2024-07-10 16:11:50 +0800

    5.14 Test if URL and likes rendered after view button is clicked

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import { expect, test } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 test('<Blog /> only renders title and author on initial render', () => {
   const blog = {
@@ -32,4 +33,33 @@ test('<Blog /> only renders title and author on initial render', () => {
 
   // The style of the pressViewRender should be display: none
   expect(viewBlogRenderDiv).toHaveStyle('display: none')
+})
+
+test('Likes and URL displayed when view button is clicked', async () => {
+  const blog = {
+    title: 'test',
+    author: 'tester',
+    url: 'test.com',
+    likes: 0,
+    user: {
+      name: 'user123',
+      username: 'user123',
+    },
+  }
+
+  const user = {
+    username: 'user123',
+  }
+
+  const { container } = render(<Blog blog={blog} user={user} />)
+  const virtualUser = userEvent.setup()
+
+  const viewButton = screen.getByText('view')
+  await virtualUser.click(viewButton)
+
+  // Isolate the div that should be displayed when view is clicked
+  const viewBlogRenderDiv = container.querySelector('.viewBlogRender')
+  // Two separate expects for clarity
+  expect(viewBlogRenderDiv).toHaveTextContent('test.com')
+  expect(viewBlogRenderDiv).toHaveTextContent('0')
 })
